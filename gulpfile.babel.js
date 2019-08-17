6cfd757020e84e4a0f0826a297298acd88a4c196
DorianDesings <dorianoriginaldesings@gmail.com>
2019-08-17 20:54:07 +0200

    initial commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,25 @@
+//Constantes para el automatizador de tareas
+const gulp = require("gulp")
+const pug = require('gulp-pug')
+const plumber = require("gulp-plumber")
+
+const browserSync = require('browser-sync')
+
+const server = browserSync.create()
+
+gulp.task('pug', () => {
+  return gulp.src('./dev/*.pug')
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest('./public/'))
+})
+
+gulp.task('default', () => {
+  server.init({
+    server: './public'
+  })
+
+  //PUG
+  gulp.watch('./dev/*.pug', gulp.series('pug')).on('change', server.reload)
+})
